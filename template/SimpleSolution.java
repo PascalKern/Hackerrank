31b767e8b6d7a23539c635cb5f9eddb1ef4126f8
Pascal Kern <kern@imagic.ch>
2015-10-28 14:07:35 +0100

    Adds a simple solution template.

---
+++ template/SimpleSolution.java
@@ -0,0 +1,49 @@
+package info.pkern;
+/* ============================================================================
+ * Copyright (c) 2015 Pascal Kern
+ * 
+ * http://github.com/PascalKern/Hackerrank_java7
+ * http://www.pkern.info/
+ * ============================================================================
+ */
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+/**
+ * @version 0.1 - Simple (Hackerrank Solution-Environment)
+ * @author Pascal Kern
+ * @category ... - ... : ...
+ * @see https://www.hackerrank.com/challenges/...
+ */
+public class Solution {
+	
+    public static void main(String[] args) {
+    	
+    	
+    	
+    }
+    
+    
+	
+	
+	
+	
+	/* ************************************************************************************************************
+	 * Classes to solve this hackerrank challenge. When working on the solution locally this
+	 * classes can be separately unit tested.
+	 **************************************************************************************************************/
+	
+	
+    
+	/* ************************************************************************************************************
+	 * General helper classes for the submission of a hackerrank solution. When working on the solution locally
+	 * those classes are available within the project. 
+	 **************************************************************************************************************/
+//    public class ListTypeConverter {
+    
+    
+}
