77333fbf93aa2ce633a803eb654e9c7e8c6afe66
Pascal Kern <kern@imagic.ch>
2015-10-29 14:46:22 +0100

    Finishes the very simple impl. of "Manasa and Stones" challenge.

--- src/info/pkern/algorithms/impl/gridSearch/manasAndStones/Solution.java
+++ src/info/pkern/algorithms/impl/gridSearch/manasAndStones/Solution.java
@@ -7,8 +7,6 @@ package info.pkern.algorithms.impl.gridSearch.manasAndStones;
  * ============================================================================
  */
 
-import info.pkern.hackerrank.tools.ListTypeConverter;
-
 import java.io.*;
 import java.util.*;
 import java.text.*;
@@ -25,12 +23,16 @@ public class Solution {
 	
     public static void main(String[] args) {
     	
-    	List<Integer> values = getPossibleValues(3, 1, 2);
-    	Collections.sort(values);
-    	System.out.println("Vals 1: " + ListTypeConverter.toSpaceSeparatedString(values));
-    	values = getPossibleValues(4, 10, 100);  
-    	Collections.sort(values);
-    	System.out.println("Vals 2: " + ListTypeConverter.toSpaceSeparatedString(values));
+    	List<Integer> values;
+    	try (Scanner scanner = new Scanner(System.in)) {
+    		int nrTestCasses = scanner.nextInt();
+    		while (nrTestCasses > 0) {
+    			values = getPossibleValues(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+    			Collections.sort(values);
+    			System.out.println(ListTypeConverter.toSpaceSeparatedString(values));
+    			nrTestCasses--;
+    		}
+    	}
     	
     }
     
@@ -49,14 +51,25 @@ public class Solution {
 	 * Classes to solve this hackerrank challenge. When working on the solution locally this
 	 * classes can be separately unit tested.
 	 **************************************************************************************************************/
-	
+
 	
     
 	/* ************************************************************************************************************
 	 * General helper classes for the submission of a hackerrank solution. When working on the solution locally
 	 * those classes are available within the project. 
 	 **************************************************************************************************************/
-//    public class ListTypeConverter {
+    private static class ListTypeConverter {
+    	public static <E> String toSpaceSeparatedString(List<E> list) {
+    		StringBuilder sb = new StringBuilder();
+    		for (E entry : list) {
+    			sb.append(entry);
+    			if (list.indexOf(entry) < list.size()) {
+    				sb.append(" ");
+    			}
+    		}
+    		return sb.toString();
+    	}
+    }
     
     
 }
