e36adfafb5090e87fa649923e4fdd9d3684e9c7e
Pascal Kern <kern@imagic.ch>
2015-11-03 10:50:14 +0100

    Adds a simple test to use the static T-SNE Demo class. Not yet working!

---
+++ exp_src/info/pkern/sandbox/ai/statistic_ml/documentClassification/localClasses/TsnePlotExample.java
@@ -0,0 +1,13 @@
+package info.pkern.sandbox.ai.statistic_ml.documentClassification.localClasses;
+
+import com.jujutsu.tsne.demos.TSneDemo;
+
+public class TsnePlotExample {
+
+	public static void main(String[] args) {
+		
+		TSneDemo.main(new String[]{"./noGit/example.txt", "./noGit/labels_example.txt"});
+		
+	}
+
+}
