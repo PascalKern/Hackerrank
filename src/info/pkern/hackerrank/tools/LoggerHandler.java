1f024a5aee3034f5e38f62d7a87647cb3ad8fab7
Pascal Kern <kern@imagic.ch>
2015-11-05 15:28:54 +0100

    Updates LoggerHandler to simple exclude packages.

--- src/info/pkern/hackerrank/tools/LoggerHandler.java
+++ src/info/pkern/hackerrank/tools/LoggerHandler.java
@@ -1,26 +1,13 @@
-package info.pkern.hackerrank.tools;
-/* ============================================================================
- * Copyright (c) 2015 Pascal Kern
- * 
- * http://github.com/PascalKern/Hackerrank_java7
- * http://www.pkern.info/
- * ============================================================================
- */
-
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
-/**
- * @version 0.1 - (Hackerrank Solution-Tools)
- * @author Pascal Kern
- */
 public class LoggerHandler {
 
-//	private static final String LOG_FORMAT = "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS.%1$tL %4$-7s [%3$s] (%2$s) %5$s %6$s%n";
-	private static final String LOG_FORMAT = "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS.%1$tL %4$-7s (%2$s) %5$s %6$s%n";
+	private static final String LOG_FORMAT = "%1$tF %1$tT,%1$tL %4$-7s [%3$s] %2$s - %5$s%6$s%n";
+//	private static final String LOG_FORMAT = "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS.%1$tL %4$-7s (%2$s) %5$s %6$s%n";
 	
 	public static void enableSingleLineFineConsoleLogging() {
 		
@@ -39,6 +26,15 @@ public class LoggerHandler {
 		}
 	}
 
+	private static void disablePackage(String packageName) {
+		Logger logger = Logger.getLogger(packageName);
+		logger.setUseParentHandlers(false);
+		//Just to secure no handler is assigned and or can log messages!
+		for (Handler handler : logger.getHandlers()) {
+			handler.setLevel(Level.OFF);
+		}
+	}
+	
 	public static void disableLogging() {
 		setAllLogHanderLevels(Level.OFF);
 	}
